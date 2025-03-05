47f86e27c8640985dff49f8af3b8a20746930bb0
Akinori Maeda <maemaeakiaki123@gmail.com>
2025-03-06 02:22:29 +0900

    Initial Commit

---
+++ stackmetronome/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  stackmetronome
+//
+//  Created by 前田哲徳 on 2025/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stackmetronome/stackmetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  stackmetronomeApp.swift
+//  stackmetronome
+//
+//  Created by 前田哲徳 on 2025/03/06.
+//
+
+import SwiftUI
+
+@main
+struct stackmetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
